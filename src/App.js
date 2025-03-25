38d6a9610b98c8f909e4b06c44b9fc86e4705776
MVLogix <mv@logixbuilt.com>
2025-03-25 18:20:15 +0530

    updated commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hii from  apache2 server 
         </p>
         <a
           className="App-link"
